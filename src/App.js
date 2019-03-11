22bfbc85d78e11d8f469da8c19b9dcedadbb2add
adeluleye <adelu4real@gmail.com>
2019-03-12 00:27:52 +0100

    implement front end form

--- src/App.js
+++ src/App.js
@@ -3,23 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    text: {
+      recipient: '',
+      textmessage: ''
+    }
+  }
+
+  sendText = _ => {
+    const { text } = this.state;
+    // pass variables within the query string
+    fetch('http://localhost:3900/send-text?recipient=${text.recipient}&textmessage=${text.textmessage}')
+    .catch(err => console.error(err))
+  }
+
   render() {
+    const { text } = this.state;
+
+    const spacer = {
+      margin: 8
+    }
+    const textArea = {
+      borderRadius: 4
+    }
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div style={{ marginTop: 10 }}>
+          <h2>Send Text Message</h2>
+          <label>Phone Number</label><br/>
+          <input value={text.recipient}
+            onChange={e => this.setState({ text: { ...text, recipient: e.target.value }})}/>
+          <div style={spacer} />
+          <label>Message</label><br/>
+          <textArea rows={3} value={text.textmessage} style={textArea} 
+            onChange={e => this.setState({ text: { ...text, textmessage: e.target.value }})}/>
+          <div style={spacer} />
+          <button onClick={this.sendText}> Send Text </button>
+        </div>
       </div>
     );
   }
